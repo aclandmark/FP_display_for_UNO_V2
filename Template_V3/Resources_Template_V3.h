97b994e2257f2cd483fce9b88d5e61461ff58d30
aclandmark <mark.osborne15@btinternet.com>
2021-07-02 16:08:31 +0100

    Create Resources_Template_V3.h

---
+++ Template_V3/Resources_Template_V3.h
@@ -0,0 +1,18 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+
+#include "Resources_Template_V3\UPDI_Programmer.h"
+#include "Resources_Template_V3\Basic_IO_and_Timer_subs.c"
+#include "Resources_Template_V3\ASKII_subroutines.c"
+#include "Resources_Template_V3\Hex_Programmer_subs.c"
+#include "Resources_Template_V3\Hex_Programmer.c"
+#include "Resources_Template_V3\UPDI_subroutines.c"
+#include "Resources_Template_V3\verify_flash.c"
+#include "Resources_Template_V3\Text_Programmer_subs.C"
