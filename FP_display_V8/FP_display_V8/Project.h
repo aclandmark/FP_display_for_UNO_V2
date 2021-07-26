7eb1677e408dad2aca8d09c35d5d375afa28190f
aclandmark <mark.osborne15@btinternet.com>
2021-07-26 12:41:43 +0100

    Update Project.h

--- FP_display_V8/FP_display_V8/Project.h
+++ FP_display_V8/FP_display_V8/Project.h
@@ -7,10 +7,9 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define display_tick 390	
-#define comms_tick	28					//27	
-#define half_comms_tick	14				//13
-
+#define display_tick	5600	//2mS assuming a clock of 2.8MHz	
+#define comms_tick		140		//20K Baud rate (2.8MHz clock not 2MHz????)	500mS per transaction
+#define half_comms_tick	70		
 
 #define wait_for_clock_tick \while (!(TCA0_SINGLE_INTFLAGS & TCA_SINGLE_CMP0EN_bm));\TCA0_SINGLE_INTFLAGS |= TCA_SINGLE_CMP0EN_bm;
 
@@ -26,14 +25,16 @@ void comms_transaction(void);
 void Display_driver(void);
 void Tx_transaction (char);
 char Rx_transaction (void);
-char RX_transaction (void);
-
 
 
 volatile char Tx_symbol, Rx_symbol, Rx_symbol_bkp;
 volatile char transaction_type;
 volatile char transaction_type = 1;
 volatile int transaction_counter = 0;
+volatile int cmp0_bkp;
+volatile char test;
+
+
 char data[10];
 
 #include "../Resources/One_wire_transactions.c"
