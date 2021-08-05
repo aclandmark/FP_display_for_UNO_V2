ed96b176e872264aa11c8b5cf2fb5488a4c2add0
aclandmark <mark.osborne15@btinternet.com>
2021-08-05 14:10:32 +0100

    Create Resources_Template_V9.h

---
+++ Template_V9/Resources_Template_V9.h
@@ -0,0 +1,22 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+
+#include "Resources_Template_V9\UPDI_Programmer.h"
+#include "Resources_Template_V9\Basic_IO_and_Timer_subs.c"
+#include "Resources_Template_V9\ASKII_subroutines.c"
+#include "Resources_Template_V9\Hex_Programmer_subs.c"
+#include "Resources_Template_V9\Hex_Programmer.c"
+#include "Resources_Template_V9\UPDI_subroutines.c"
+#include "Resources_Template_V9\verify_flash.c"
+#include "Resources_Template_V9\Text_Programmer_subs.c"
+
+
+#include "Resources_Template_V9\One_wire_transactions.h"
+#include "Resources_Template_V9\One_wire_transactions.c"
