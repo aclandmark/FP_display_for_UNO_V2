5e838668f259fe5009dda8a028225a6b4ede9f4c
aclandmark <mark.osborne15@btinternet.com>
2021-08-09 20:33:01 +0100

    Update Project.h

--- FP_display_V9/FP_display_V9/Project.h
+++ FP_display_V9/FP_display_V9/Project.h
@@ -24,8 +24,8 @@ void UART_transaction(void);
 void comms_transaction(void);
 
 void Display_driver(void);
-void Tx_transaction (char);
-char Rx_transaction (void);
+void Transmit_data_byte (char);
+char Receive_data_byte (void);
 void Char_definition(void);
 
 volatile char Tx_symbol, Rx_symbol, Rx_symbol_bkp;
@@ -37,15 +37,34 @@ volatile char cal_factor;
 volatile char test_symbol= 1;
 char data[10];
 
-char display_buffer[12];
-char temp_buffer[8];
+char display_buffer[15];		//12
+char temp_buffer[15];		//8
 volatile int display_ptr;
 volatile long Long_Num_from_UNO, Long_Num_to_UNO;
 volatile char cr_keypress;
 unsigned char data_byte[4];
 volatile int data_byte_ptr;
+volatile float Float_Num_to_UNO, Float_Num_from_UNO;
+volatile char * char_ptr, * char_ptr_2;
+volatile float * float_ptr, *float_ptr_2;
+
+#define display_buffer2temp \
+null_bit_counter = 0; \
+clear_temp_buffer;\
+for(int m = 0; m <= 14; m++)\
+{if(!(display_buffer[14-m]))null_bit_counter += 1; else break;}\
+for(int m = 0; m <= 14-null_bit_counter; m++)\
+temp_buffer[m] = display_buffer[14-null_bit_counter - m];
+
+#define temp2display_buffer \
+null_bit_counter = 0;\
+for(int m = 0; m <= 14; m++)\
+{if(!(temp_buffer[14-m]))null_bit_counter += 1; else break;}\
+for(int m = 0; m <= 14-null_bit_counter; m++)\
+display_buffer[m] = temp_buffer[14-null_bit_counter - m];
+
 
 #include "../Resources/One_wire_transactions.c"
-#include "../Resources/Display_header_V2A.h"
-
+#include "../Resources/Display_header.h"
+#include "../Resources/FPN_subroutines.c"
 
