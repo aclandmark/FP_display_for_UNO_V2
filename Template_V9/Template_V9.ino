bb198919b6d53a6b55d73634fbb71dd1680c659e
aclandmark <mark.osborne15@btinternet.com>
2021-08-31 18:25:49 +0100

    Update Template_V9.ino

--- Template_V9/Template_V9.ino
+++ Template_V9/Template_V9.ino
@@ -1,33 +1,31 @@
 
- 
-#define device_type 1606
-//long Int_from_KBD(void);
+
 
 #include "Resources_Template_V9.h"
 #include "Resources_UPDI_programmer.h"
 
-char mode = 0;
+/*
+ * Programmer uses PC0 which is initialsed to Hi-Z input
+ * Floating point display template uses PC4 for sinlge wire UART. It is also Hi-Z
+ * All other ports are WPU
+*/
 
-//ALL ports except the programming port are WPU, the programming port is tri-tate
 
 int main (void){ 
-char keypress, line_counter;
+  
+char keypress;
 long int_num;
 float FPN;
-float test = 2.0;
 char FP_string[12];
 
 
+setup_328_HW;                                           //see "Resources\ATMEGA_Programmer
 
 
-setup_328_HW;                                           //see "Resources\ATMEGA_Programme
-PORTC &= (~(1 << PORTC4));                               //One way comms for template requires port to be set to Tri state   
-P_counter = 0;                                          //Used to check parity of data received from UPDI
-
-
+/************************Programmer code starts here and can be removed************************************/
 User_prompt;
 
-/*************************************************/
+
 
 sendString("\r\nPress 'a' to program target or AOK to run taget code\r\n\r\n");
 if(waitforkeypress() == 'a'){
@@ -66,22 +64,30 @@ sendString("\r\nFuses:\tWDT, BOD, OSC,SYS0\tSYS1, APPEND, BOOTEND\r\n\t");
 read_out_fuses();
 
 UPDI_reset; 
-Dissable_UPDI_sesion;
-}
-
-/*************************************************/
-
-/************Code in this section is test code.  It is not part of the UPDI programmer******************
-******I*****Its purpose is to relay signals from the UNO UART port to the ATtiny UART ****************
-******************so that the operation of the progammer can be tested easily*************************/
+Dissable_UPDI_sesion;}
 
 
-sendString("\r\nRun trial application? -y- or AOK (POR may be required)\r\n\r\n");
+//************Code for the UNO Floating point display is presented in the next section *************************
+//**********************It is combined with the 1606 programmer for convenience**********************************
+//*****************When development is complete the two applications can be separated***********************
+
+
+
+sendString("\r\n\r\nRun trial application? -y- or AOK (POR may be required)\r\n\r\n");
 if(waitforkeypress() == 'y'){
-//PORTC &= (~(1 << PORTC4));                                                              //Tri state   The default state
+
+/************************Programmmer code ends here*******************************************************************/
+
+
+PCICR |= (1 << PCIE1); PCMSK1 |= (1 << PCINT11);                            //Set up PCI for SM switch
+PORTC &= (~(1 << PORTC4));                                                  //One way comms for template requires port to be set to Tri state 
+One_wire_Tx_char = 'F'; UART_Tx_1_wire();                                   //Command to reset 1606
 
 
 while(1){
+
+sei();                                                                      //Required to control intensity
+  
 User_prompt_template;
 
 if (User_response =='i'){
@@ -90,7 +96,7 @@ sendString("Enter integer from KBD (terminate in cr)\r\n");
 while (int_num = Int_from_KBD()){
 sendString("AK to do arithmetic\r\n");
 waitforkeypress();
-Long_Num_to_UNO = Long_Num_from_UNO * 2;
+Long_Num_to_UNO = Long_Num_from_UNO * 2;                                    //Simple arithmetic to check operation of binary/string conversions
 send_int_num(Long_Num_to_UNO);
 sendString("New number?\r\n");}}
 
@@ -98,148 +104,13 @@ if (User_response =='f'){
  sendString("Enter FPN from KBD (terminate in cr)\r\n"); 
  FPN =  Float_from_KBD();
  waitforkeypress();
- FPN = FPN * test;
-send_float_num(FPN);
-  ftoaL(FPN, FP_string);
-for(int m = 0; m <=11; m++)if(FP_string[m])sendChar(FP_string[m]); }
+ FPN = FPN * 2.75;                                                          //Simple arithmetic to check operation of binary/string conversions
+send_float_num(FPN);}}
 
-if (User_response =='r'){One_wire_Tx_char = 'F'; UART_Tx_1_wire();}
-}}
-
+}       //Only required if programmer code is included
 /**********************************End to test code section*********************************************/
 SW_reset;
 return 1;}
 
 
-
-
-/*
-Programmer subroutiness
- */
-
-/***************************************************************************************************************************************/
-void ftoaL(float Fnum, char FP_string[]){
-  int afterpoint = 0;
-  long ipart, Fnum_int;
-  char sign = '+';
-  signed char expt;
-  
-  if (Fnum < 0){sign = '-'; Fnum *= (-1);}                  //Convert negative numbers to positive ones and set the sign character
-  
-  for(int m = 0; m <= 15; m++) FP_string[m] = 0;                //Clear the floating point array
-  
-  Fnum_int = (long)Fnum;                            //Obtain integer part of the number
-  
-  if (Fnum_int < 10)afterpoint = 5;                     //Number of decimal places is matched to display length
-  if ((Fnum_int >= 10) && (Fnum_int < 100))afterpoint = 4;
-  if ((Fnum_int >= 100) && (Fnum_int < 1000))afterpoint = 3;
-  if ((Fnum_int >= 1000) && (Fnum_int < 10000))afterpoint = 2;
-  
-  expt = 0;                                 //Convert very large and small numbers to scientific form
-  if (Fnum  >= 10000) {while (Fnum >= 10)
-  {Fnum /= 10; expt += 1;}afterpoint = 5;}
-  
-  if(Fnum < 0.01) {while (Fnum < 1){Fnum *= 10; expt -= 1;}}
-  
-                                        //FP to askii routines taken from "https://www.geeksforgeeks.org/convert-floating-point-number-string/"
-  ipart = (long)Fnum;                             //Obtain integer part of FP number
-  float fpart = Fnum - (float)ipart;                      //Obtain floating part
-  long i = longToStr(ipart, FP_string, 0);                  //Convert integer part to string
-
-  if (afterpoint != 0){                           //Add Decimal part to the string
-    FP_string[i] = '.';
-    fpart = fpart * pow(10,afterpoint);
-  longToStr((long)fpart, FP_string + i + 1, afterpoint);}
-
  
-  //expt = Format_for_Display(FP_string, sign, expt);
-}
-
-
-
-/***************************************************************************************************************************************/
-long longToStr(long x, char str[], int d)
-{
-  int i = 0;
-  while (x)
-  {   str[i++] = (x%10) + '0';
-  x = x/10; }
-  
-  while (i < d)
-  str[i++] = '0';
-  reverse(str, i);
-  str[i] = '\0';
-return i; }
-
-
-/***************************************************************************************************************************************/
-void reverse(char *str, int len)
-{
-  int i=0, j=len-1, temp;
-  while (i<j)
-  {   temp = str[i];
-    str[i] = str[j];
-    str[j] = temp;
-  i++; j--; }}
-
-  
-
-
-
-
-/*******************************************************************************************************************************/
-void UART_Tx(unsigned int data_byte_T){               //starts Hi Z
-  unsigned char parity = 0;
-  DDRC |= (1 << DDC0);                                //start bit
-  clock_delay_T;
-
-for (int n = 0; n <= 7; n++){
-  if (data_byte_T & (1 << n))
-  {DDRC &= (~(1 << DDC0)); parity += 1;}
- else {DDRC |= (1 << DDC0);}clock_delay_T;}           //clock data out
-
-if (parity%2){DDRC &= (~(1 << DDC0));} 
-else {DDRC |= (1 << DDC0);}
-  clock_delay_T;                                      //clock parity bit
-  DDRC &= (~(1 << DDC0));                             //initiate stop bit 
-  clock_delay_T;                                      //Stop bits
-  clock_delay_T;}
-
-
-/************************************************************************************************/
-unsigned char UART_Rx(void){
-  unsigned char data_byte_R = 0;
-    char parity = 0;
-                          
-  while (PINC & (1 << PINC0));                        //wait for start bit
-  clock_delay_R;                                      //The full delay is required 
-  for (int n= 0; n <= 7; n++){clock_delay_R;
-    if (PINC & (1 << PINC0))
-    {data_byte_R |= (1 << n); parity += 1;}}
-    clock_delay_R;
-
- if ((PINC & (1 << PINC0)) && (parity%2)); 
- else P_counter++;                                      //Parity often received in error?????
- 
- clock_delay_R;                                         //stop bit
- clock_delay_R;                                         //stop bit  
- return data_byte_R;}
-
-
-/*********************************************************************************/
-ISR(USART_RX_vect){if (mode =='H')upload_hex();}
-//                    if (mode =='T')upload_text();}
-                    
-ISR(TIMER1_OVF_vect) {
-  if(!(T1OVFM)){UPDI_timeout = 1;TCCR1B = 0;}
-  
-  if(T1OVFM == 1){
-    if(text_started == 3){T1OVFM = 0;
-    endoftext -= 1; 
-  TCCR1B = 0; TIMSK1 &= (~(1 << TOIE1));       
-  inc_w_pointer; store[w_pointer] = 0;                         
-  inc_w_pointer; store[w_pointer] = 0;}}}
-
-
- /**********************************************************************************************************************/
- /***************************************************************************************************************************************/
