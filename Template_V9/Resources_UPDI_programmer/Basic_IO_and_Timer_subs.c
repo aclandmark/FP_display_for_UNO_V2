e26462ca044b2cf22700d1cb14d82f2ec2a80be5
aclandmark <mark.osborne15@btinternet.com>
2021-09-02 15:40:51 +0100

    Update Basic_IO_and_Timer_subs.c

--- Template_V9/Resources_UPDI_programmer/Basic_IO_and_Timer_subs.c
+++ Template_V9/Resources_UPDI_programmer/Basic_IO_and_Timer_subs.c
@@ -1,57 +1,29 @@
 
-void USART_init (unsigned char, unsigned char);
+void Timer_T0_sub(char, unsigned char);
+char receiveChar(void);
+void sendChar(char);
 
 void Timer_T0_10mS_delay_x_m(int);
-void Timer_T0_sub(char, unsigned char);
 void Timer_T0_sub_with_interrupt(char, unsigned char);
-
 void Timer_T1_sub(char, unsigned int);
 void Timer_T1_sub_with_interrupt(char, unsigned int);
-
 void Timer_T2_sub(char, unsigned char);
 void Timer_T2_sub_with_interrupt(char, unsigned char); 
 
 void newline(void);
-char waitforkeypress(void);
-char receiveChar(void);
-char isCharavailable (int);
-
 void binUnwantedChars (void);
-void sendChar(char);
 void sendString_with_pause(const char*);
-void sendString(const char*);
 void NumericString_to_PC_with_pause(char*);
 
 
 
 
 
-/**********************************************************************************************/
-void USART_init (unsigned char UBRROH_N, unsigned char UBRR0L_N ){
-Timer_T0_sub(T0_delay_5ms);
-UCSR0B = 0;
-UBRR0H = UBRROH_N; 
-UBRR0L = UBRR0L_N;  
-UCSR0A = (1 << U2X0);
-UCSR0B = (1 << RXEN0) | (1<< TXEN0);
-UCSR0C =  (1 << UCSZ00)| (1 << UCSZ01);} 
-
 
 
 /**********************************************************************************************/
 void Timer_T0_10mS_delay_x_m(int m)
 {for (int n = 0; n < m; n++){Timer_T0_sub(T0_delay_10ms);}}
-
-
-
-/**********************************************************************************************/
-void Timer_T0_sub(char Counter_speed, unsigned char Start_point){ 
-
-GTCCR | (1 << PSRSYNC);
-TCNT0 = Start_point;
-TCCR0B = Counter_speed;
-while(!(TIFR0 & (1<<TOV0)));
-TIFR0 |= (1<<TOV0); TCCR0B = 0;}
 
 
 
@@ -115,36 +87,8 @@ void newline(void){sendString ("\r\n");}
 
 
 /**********************************************************************************************/
-char waitforkeypress(void){
-while (!(UCSR0A & (1 << RXC0)));
-return UDR0;}
-
-
-
-/**********************************************************************************************/
-char receiveChar(void)
-{return UDR0;}
-
-
-
-/**********************************************************************************************/
-char isCharavailable (int m){int n = 0;		
-while (!(UCSR0A & (1 << RXC0))){n++;
-if (n>8000) {m--;n = 0;}if (m == 0)return 0;}
-return 1;}
-
-
-
-
-/**********************************************************************************************/
 void binUnwantedChars (void){char bin_char;
 while(1){if (isCharavailable(5)==1)bin_char = receiveChar();else break;}}
-
-
-/**********************************************************************************************/
-void sendChar(char data){
-while (!(UCSR0A & (1 << UDRE0)));
-UDR0 = data;}
 
 
 
@@ -159,12 +103,6 @@ sendChar(s[i++]);Timer_T0_sub(T0_delay_2ms);} }
 
 
 
-/**********************************************************************************************/
-void sendString(const char s[]){
-int i = 0;
-while(i < 200){
-if(s[i] == '\0') break;
-sendChar(s[i++]);} }
 
 
 /*********************************************************************/
