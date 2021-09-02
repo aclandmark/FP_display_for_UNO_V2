507f06248423dce7fa770933d2a5d393ed3ca49f
aclandmark <mark.osborne15@btinternet.com>
2021-09-02 15:40:59 +0100

    Update UPDI_Programmer.h

--- Template_V9/Resources_UPDI_programmer/UPDI_Programmer.h
+++ Template_V9/Resources_UPDI_programmer/UPDI_Programmer.h
@@ -206,35 +206,8 @@ volatile char Rx_askii_char_old;                             //Required to check
 int address_in_flash;                                       //Address in flash at which text data is to be programmed
 unsigned char  op_code;
 
-char User_response;
+
 volatile char T1OVFM = 0;
-
-
-
-/************************************************************************************************************************************/
-#define setup_328_HW \
-\
-setup_watchdog;\
-ADMUX |= (1 << REFS0);\
-initialise_IO;\
-\
-USART_init(0,68);\
-FlashSZ = 0x2000;
-
-
-
-/************************************************************************************************************************************/
-#define wdr()  __asm__ __volatile__("wdr")
-
-
-#define setup_watchdog \
-wdr();\
-MCUSR &= ~(1<<WDRF);\
-WDTCSR |= (1 <<WDCE) | (1<< WDE);\
-WDTCSR = 0;
-
-
-#define SW_reset {wdt_enable(WDTO_30MS);while(1);}
 
 
 
@@ -264,19 +237,6 @@ r_pointer = r_pointer & 0b00111111;
 
 
 
-
-
-/************************************************************************************************************************************/
-#define initialise_IO \
-MCUCR &= (~(1 << PUD));\
-DDRB = 0;\
-DDRC = 0;\
-DDRD = 0;\
-PORTB = 0xFF;\
-PORTC = 0xFE;\
-PORTD = 0xFF;
-
-//PORTC0 (the programming port) stays as Hi Z input
 
 
 
